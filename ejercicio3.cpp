7ba1c6919f9de3156a458f0d5474a7caa1d9f387
YakelinRomeroG <159857495+YakelinRomeroG@users.noreply.github.com>
2024-02-26 22:49:41 -0500

    Add files via upload

---
+++ ejercicio3.cpp
@@ -0,0 +1,25 @@
+/*un rpograma que lea  un mes y un dia de dicho mes para luego
+ * deir si esa combinacion de mes y di son validos.
+ * el caso mas especial es el 29 de febrero,
+ * en dicho caso imprimir"posible año bisiesto"
+ * el formato de salida debe ser: 14 es un mes invalido.
+ * 31/4 es una fecha invalida.
+ * 27/4 es una fecha valida.
+ * 29/2 es valida en bisiesto.*/
+
+#include<iostream>
+
+int main(){
+    char mes;
+    //verificar el mes
+
+
+    //verificar el dia
+
+
+    //
+
+}
+
+
+
---
+++ ejercicio4.cpp
@@ -0,0 +1,9 @@
+/*Escriba un rpograma para leer dos numeros enteros con el siguiente significado:
+ * el valor del primer numero representa una hora del dia en un reloj de 24 horas, de modo
+ * que 1245 representa las 12:45 de la tarde. el segundo ntero representa
+ * un tiempo de duracion de la misma manera, por lo que 345 reprenta tres horas 45 minutos.
+ * el programa debe sumar esta duracion, y el resultado sera impreso en la}misma notacion,
+ * en este caso 1630, que es el tiempo de 3 horas y 45 minutos despues de 12:45.
+ * nota: el formato debe ser: La hora es 1630.
+ * otra nota: se debe imprimir un msj como el siguiente cuando los datos ingresados en
+ * invalido: 1560 es un tiempo invalido.*/
