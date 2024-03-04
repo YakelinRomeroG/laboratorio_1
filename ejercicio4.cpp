1124d65ce10e5e5e001a38c19b74d9c1cffd7057
YakelinRomeroG <159857495+YakelinRomeroG@users.noreply.github.com>
2024-03-04 15:56:49 -0500

    Update ejercicio4.cpp

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -7,3 +7,30 @@
  * nota: el formato debe ser: La hora es 1630.
  * otra nota: se debe imprimir un msj como el siguiente cuando los datos ingresados en
  * invalido: 1560 es un tiempo invalido.*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int hora_1, hora_2;
+
+    cout << "Ingrese la hora #1 (formato 24 horas sin : ) : "; //usuario ingresa la hora
+    cin >> hora_1;
+
+    cout << "Ingrese la hora de duración (formato 24 horas sin : ) : "; // usuario ingresa la duracion
+    cin >> hora_2;
+
+    // Sumamos las horas
+    int total_horas1 = (hora_1 % 100) + (hora_2 % 100);
+    int total_horas2 = (hora_1 / 100) + (hora_2 / 100) + (total_horas1 / 60);
+    total_horas1 %= 60;
+
+    // Verificar si los datos ingresados son válidos
+    if ((hora_1 < 0 || hora_1 >= 2400 || hora_1 % 100 >= 60) || (hora_2 < 0 || hora_2 >= 2400 || hora_2 % 100 >= 60)) {
+        cout << " es un tiempo inválido." << endl;
+        }
+        else{
+            cout << "La hora es " << total_horas2 << total_horas1 << "." << endl;
+        }
+    return 0;
+}
