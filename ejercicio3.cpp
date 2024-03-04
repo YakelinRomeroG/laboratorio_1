7cba0b3b2fda3ff73142113287db2a204af700d7
YakelinRomeroG <159857495+YakelinRomeroG@users.noreply.github.com>
2024-03-04 15:55:01 -0500

    Update ejercicio3.cpp

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -8,17 +8,64 @@
  * 29/2 es valida en bisiesto.*/
 
 #include<iostream>
+using namespace std;
 
 int main(){
-    char mes;
-    //verificar el mes
+    int  mes,dia;
 
+    cout<<"Ingrese el número del mes (1-12): "<<endl; // el usuario ingresa el numero del mes
+    cin>> mes;
+    cout<< "Ingrese el número del día: " << endl;  // El usuario ingresa el dia de la fecha
+    cin>>dia;
+    switch (mes) {  //usamos la estructura switch para evaluar el mes y dia ingresados en sus distintos casos.
+    case 2: // mes de febrero
+            if (dia >= 1 && dia <= 28) {
+                cout << dia << "/" << mes << " es una fecha válida." << endl;
+            } else if (dia == 29){
+                cout << dia << "/" << mes << " es una fechavalida en bisiesto." << endl;
+            } else {
+                cout << dia << "/" << mes << " es una fecha inválida." << endl;
+            }
+            break;
+    case 4: // mes de abril
+            if (dia >= 1 && dia <= 31) {
+                cout << dia << "/" << mes << " es una fecha válida." << endl;
+            } else {
+                cout << dia << "/" << mes << " es una fecha inválida." << endl;
+            }
+            break;
+    case 6:  // mes de junio
+            if (dia >= 1 && dia <= 31) {
+                cout << dia << "/" << mes << " es una fecha válida." << endl;
+            } else {
+                cout << dia << "/" << mes << " es una fecha inválida." << endl;
+            }
+            break;
+    case 9:  // mes de septiembre
+            if (dia >= 1 && dia <= 31) {
+                cout << dia << "/" << mes << " es una fecha válida." << endl;
+            } else {
+                cout << dia << "/" << mes << " es una fecha inválida." << endl;
+            }
+            break;
+    case 11:  // mes de noviembre
+            if (dia >= 1 && dia <= 31) {
+                cout << dia << "/" << mes << " es una fecha válida." << endl;
+            } else {
+                cout << dia << "/" << mes << " es una fecha inválida." << endl;
+            }
+            break;
 
-    //verificar el dia
+    default: // evalua los meses enero,marzo,mayo, julio,agosto,octubre y diciembre.
+        if (dia >= 1 && dia <= 31) {
+            cout << dia << "/" << mes << " es una fecha válida." << endl;
+        } else {
+            cout << dia << "/" << mes << " es una fecha inválida." << endl;
+        }
+        break;
 
-
-    //
-
+    }
+    return 0;
 }
 
 
